bd799d98d7afbe645b3645cf8733956af1fd587f
uhammgm <uhammgm@stu.ccsdut.org>
2023-05-19 10:55:32 -0600

    Kewl

--- Task 1/features.py
+++ Task 1/features.py
@@ -1,7 +1,10 @@
 import turtle 
 class features:
+
+    def __init__(self):
+        print("I have been created.")
     
-    def drawHead():
+    def drawHead(self):
         turtle.penup()
         turtle.goto(200 , 0)
         turtle.setheading(90)
@@ -14,7 +17,7 @@ class features:
         turtle.width(2)
         turtle.circle(200)
     
-    def drawEyes():
+    def drawEyes(self):
         turtle.penup()
         turtle.goto(100 , 75)
         turtle.color("black")
@@ -28,20 +31,47 @@ class features:
         turtle.circle(25)
         turtle.end_fill()
 
-    def drawSmile():
+    def drawSmile(self):
         turtle.penup()
+        turtle.color("black")
         turtle.goto(-145 , -65)
         turtle.pendown()
         turtle.setheading(310)
         turtle.width(5)
         turtle.circle(200,100)
 
-    def drawSad():
+    def drawSad(self):
         turtle.penup()
+        turtle.color("black")
         turtle.goto(150 , -115)
         turtle.pendown()
         turtle.setheading(130)
         turtle.width(5)
         turtle.circle(200,100)
         
- 
+    def drawEyesBlue(self):
+        turtle.penup()
+        turtle.goto(100 , 75)
+        turtle.color("blue")
+        turtle.begin_fill()
+        turtle.circle(25)
+        turtle.end_fill()
+        turtle.penup()
+        turtle.goto(-50 , 75)
+        turtle.color("blue")
+        turtle.begin_fill()
+        turtle.circle(25)
+        turtle.end_fill()
+
+    def drawHeadMad(self):
+        turtle.penup()
+        turtle.goto(200 , 0)
+        turtle.setheading(90)
+        turtle.pendown()
+        turtle.color("red")
+        turtle.begin_fill()
+        turtle.circle(200)
+        turtle.end_fill()
+        turtle.color("black")
+        turtle.width(2)
+        turtle.circle(200)
--- Task 1/main.py
+++ Task 1/main.py
@@ -2,22 +2,66 @@ import turtle
 from features import *
 
 turtle.speed(100)
-features.drawHead()
-features.drawEyes()
-features.drawSmile()
-turtle.done()
+drawer = features()
+drawer.drawHead()
+drawer.drawEyes()
+drawer.drawSmile()
+
+mouthStat = 1
+headStat = 1
+eyesStat = 1
+FroSmi = "1.Make me sad"
+EyeBlue = "2.Make my eyes blue"
+HapMad = "3.Make me mad"
 
 choice = 0
 finish = False
 while finish != True:    
-    choice = int(input("Change me!\n\t1.Make me sad\n\t4.Exit "))
-
+    print("Change me!" + "\n\t" + FroSmi + "\n\t" + EyeBlue + "\n\t" + HapMad + "\n\t4.Exit ")
+    choice = int(input("Choice: "))
     
     if choice == 1:
-        features.drawHead()
-        features.drawEyes()
-        features.drawSad()
-        turtle.done()
+        if headStat == 1:
+            drawer.drawHead()
+        elif headStat == 2:
+            drawer.drawHeadMad()
+        if eyesStat == 1:
+            drawer.drawEyes()
+        elif eyesStat == 2:
+            drawer.drawEyesBlue()
+        drawer.drawSad()
+        mouthStat = 2
+        FroSmi = "1.Make me smile"
+        
+
+    if choice == 2:
+        eyesStat = 2
+        if headStat == 1:
+            drawer.drawHead()
+        elif headStat == 2:
+            drawer.drawHeadMad()
+        if eyesStat == 1:
+            drawer.drawEyes()
+        elif eyesStat == 2:
+            drawer.drawEyesBlue()
+        if mouthStat == 1:
+            drawer.drawSmile()
+        elif mouthStat == 2:
+            drawer.drawSad()
+        EyeBlue = "2.Make my eyes black"
+
+    if choice == 3:
+        drawer.drawHeadMad()
+        if eyesStat == 1:
+            drawer.drawEyes()
+        elif eyesStat == 2:
+            drawer.drawEyesBlue()
+        if mouthStat == 1:
+            drawer.drawSmile()
+        elif mouthStat == 2:
+            drawer.drawSad()
+        headStat = 2
+        HapMad = "3.Make me happy"
 
     if choice == 4:
         finish = True
